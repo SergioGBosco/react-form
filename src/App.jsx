943f839f5e401982814b6c7852ec35683dcb4d19
SergioGBosco <sergiogibosco@gmail.com>
2025-08-29 16:38:23 +0200

    feat: add array for create articles blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-
+const articles = [
+  {
+    id: 1,
+    title: "Viaggio in Silia"
+  },
+  {
+    id: 2,
+    title: "Vivere di rendita"
+  },
+  {
+    id: 3,
+    title: "C'è vita oltre la terra?"
+  },
+  {
+    id: 4,
+    title: "Navigare per mare, la storia della navigazione"
+  },
+  {
+    id: 5,
+    title: "L'assicurazione auto più vantaggiosa in Italia"
+  },
+];
 
 function App() {
 
 
   return (
 
-    <div>Esercizio Form</div>
+    <div className="container">
+      <div className="row">
+        {articles.map((article) => {
+          return (
+            <div className="col-100">
+              <div className="article">
+                <h3>{article.title}</h3>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
