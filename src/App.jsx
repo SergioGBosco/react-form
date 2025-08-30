26e88282b2aa8a02a8d4fe176fe0f3fdc3d48bb6
SergioGBosco <sergiogibosco@gmail.com>
2025-08-30 17:20:47 +0200

    feat: Add method for add new object in the neww array with useState, fix cicle articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,9 @@
-const articles = [
-  {
-    id: 1,
-    title: "Viaggio in Silia"
-  },
-  {
-    id: 2,
-    title: "Vivere di rendita"
-  },
-  {
-    id: 3,
-    title: "C'è vita oltre la terra?"
-  },
-  {
-    id: 4,
-    title: "Navigare per mare, la storia della navigazione"
-  },
-  {
-    id: 5,
-    title: "L'assicurazione auto più vantaggiosa in Italia"
-  },
-];
-
+import ArticlesList from "./ArticlesList"
 function App() {
 
 
   return (
-
-    <div className="container">
-      <div className="row">
-        {articles.map((article) => {
-          return (
-            <div className="col-100">
-              <div className="article">
-                <h3>{article.title}</h3>
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <ArticlesList />
   )
 }
 
---
+++ src/ArticlesList.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+const articles = [
+  {
+    id: 1,
+    title: "Viaggio in Silia"
+  },
+  {
+    id: 2,
+    title: "Vivere di rendita"
+  },
+  {
+    id: 3,
+    title: "C'è vita oltre la terra?"
+  },
+  {
+    id: 4,
+    title: "Navigare per mare, la storia della navigazione"
+  },
+  {
+    id: 5,
+    title: "L'assicurazione auto più vantaggiosa in Italia"
+  },
+];
+
+
+const ArticlesList = () => {
+  const [arts, setArts] = useState(articles)
+  const [addArt, setAddArt] = useState("Aggiungi Articolo")
+
+
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    let NewArt = {
+      id: articles[articles.length - 1].id + 1,
+      title: addArt,
+    };
+    // kCreata copia dell'arry originale 
+    const copyArticles = [...arts, NewArt];
+    setArts(copyArticles)
+  }
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-100">
+          <form onSubmit={handleSubmit}>
+            <input type="text"
+              className="addArticles"
+              placeholder="Aggiungi un Articolo..."
+              value={addArt}
+              onChange={e => { setAddArt(e.target.value) }} />
+
+          </form>
+        </div>
+
+        {/* Devo ciclare l'array che corrisponde allo stato attuale */}
+        {arts.map((article) => {
+          return (
+            <div className="col-100" key={article.id}>
+              <div className="article">
+                <h3>{article.title}</h3>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default ArticlesList
